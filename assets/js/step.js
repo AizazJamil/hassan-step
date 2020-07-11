8e199395383d306b5e4ed31028e3117606145c38
AizazJamil <aizaz-jamil28@outlook.com>
2020-07-12 00:57:58 +0500

    Initial Commit

--- assets/js/step.js
+++ assets/js/step.js
@@ -16,7 +16,7 @@ $(function() {
       instance.addClass('out');
       instance.siblings('button[name="finish"]').removeClass('out');
     }
-    $(stepItem[step]).addClass('active');
+    $(stepItem[step]).addClass('activeItem');
     $('.step-content').addClass('out');
     step++;
     $('#' + stepItem[step].dataset.id).removeClass('out');
@@ -27,7 +27,7 @@ $(function() {
     if (step == stepItem.length) {
       return;
     }
-    $(stepItem[stepItem.length - 1]).addClass('active');
+    $(stepItem[stepItem.length - 1]).addClass('activeItem');
     $('.step-content').addClass('out');
     $('#stepLast').removeClass('out');
     step++;
@@ -47,12 +47,12 @@ $(function() {
     $('.step-content').addClass('out');
     $('#' + stepItem[step].dataset.id).removeClass('out');
     if (step === 0) {
-      stepItem.removeClass('active');
+      stepItem.removeClass('activeItem');
     } else {
-      stepItem.filter(':gt(' + (step - 1) + ')').removeClass('active');
+      stepItem.filter(':gt(' + (step - 1) + ')').removeClass('activeItem');
     }
     if (step - 1 < 0) {
-      $('.step-content .step-content-foot button[name="prev"]').addClass('out');
+      $('.step-content .step-button button[name="prev"]').addClass('out');
     }
   });
 });
@@ -68,3 +68,43 @@ experienceFormButton.addEventListener('click', () => {
   experienceFormButton.classList.add('add-experience-none');
   dividerBar.style.display = 'none';
 })
+
+
+// Education Form Show and Hide Code here.
+const educationForm = document.querySelector('.education-field-form');
+const educationFormButton = document.querySelector('.add-education');
+// const dividerEducationBar = document.querySelector('.divider-border-dotted');
+
+educationFormButton.addEventListener('click', () => {
+  educationForm.classList.add('educationForm-field-block');
+  educationFormButton.classList.add('add-education-none');
+  dividerBar.style.display = 'none';
+})
+
+// Skill Form Show and Hide Code here.
+const skillForm = document.querySelector('.skill-field-form');
+const skillFormButton = document.querySelector('.add-skill');
+// const dividerEducationBar = document.querySelector('.divider-border-dotted');
+
+skillFormButton.addEventListener('click', () => {
+  skillForm.classList.add('skillForm-field-block');
+  skillFormButton.classList.add('add-skill-none');
+  dividerBar.style.display = 'none';
+})
+
+
+// Upload Image Profile/Avatar
+function imageProfilePreview(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            $('#previewProfileUpload').css('background-image', 'url('+e.target.result +')');
+            $('#previewProfileUpload').hide();
+            $('#previewProfileUpload').fadeIn(650);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+$("#imageUpload").change(function() {
+    imageProfilePreview(this);
+});
